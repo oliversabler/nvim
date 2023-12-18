a1ff879aca2e0d3f3629e8d8b69fd22d0265519a
oliversabler <oliver.sabler@gmail.com>
2023-12-18 15:27:39 +0100

    Add remaps for moving line(s) up and down

--- lua/chaebol/remap.lua
+++ lua/chaebol/remap.lua
@@ -6,6 +6,10 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
+-- Move line(s) up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- Copy to clipboard
 vim.keymap.set('v', '<leader>y', '"+y')
 vim.keymap.set('n', '<leader>Y', '"+Y')
