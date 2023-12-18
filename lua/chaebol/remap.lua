aaa2cbecb159a32e729cdf03499dcfc9ea76e8e6
oliversabler <oliver.sabler@gmail.com>
2023-12-18 13:43:37 +0100

    Add copy to clipboard keymaps

--- lua/chaebol/remap.lua
+++ lua/chaebol/remap.lua
@@ -5,3 +5,7 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Copy to clipboard
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
