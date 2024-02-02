1f8f5bbefe9339d29ad344ddba664eab4df3bfac
oliversabler <oliver.sabler@gmail.com>
2024-02-02 19:46:15 +0100

    Set group for autocmds

--- lua/chaebol/autocmd.lua
+++ lua/chaebol/autocmd.lua
@@ -1,12 +1,17 @@
+local format_group = vim.api.nvim_create_augroup('chaebol_format', { clear = true })
+
 -- Remove trailing whitespace on save
 vim.api.nvim_create_autocmd('BufWritePre', {
+    group = format_group,
     pattern = '*',
     command = [[%s/\s\+$//e]],
 })
 
+-- Format go-files  on save
 vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.go',
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end
+    group = format_group,
+    pattern = '*.go',
+    callback = function()
+        vim.lsp.buf.format({ async = false })
+    end
 })
