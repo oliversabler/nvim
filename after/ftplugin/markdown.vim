cc4725bcb666a3942154eb47ada4d7f67d130a85
oliversabler <oliver.sabler@gmail.com>
2025-04-03 10:27:58 +0200

    replace buggy markdown settings with toggle function

--- after/ftplugin/markdown.vim
+++
@@ -1,3 +0,0 @@
-setlocal columns=102
-setlocal wrap
-setlocal linebreak
--- lua/chaebol/remap.lua
+++ lua/chaebol/remap.lua
@@ -4,6 +4,27 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
+-- Toggle wrapping
+local term_width = vim.opt.columns
+local toggle_wrapping = false
+vim.keymap.set('n', '<C-m>', function ()
+    if toggle_wrapping then
+        vim.opt.columns = term_width
+        vim.opt.wrap=false
+        vim.opt.linebreak=false
+
+        vim.cmd('redraw')
+        vim.opt.colorcolumn =  '100'
+    else
+        vim.opt.columns= 102
+        vim.opt.colorcolumn = {}
+        vim.opt.wrap=true
+        vim.opt.linebreak=true
+    end
+
+    toggle_wrapping = not toggle_wrapping
+end)
+
 -- Move line(s) up/down
 vim.keymap.set('v', '<C-k>', ":m '<-2<CR>gv=gv")
 vim.keymap.set('v', '<C-j>', ":m '>+1<CR>gv=gv")
