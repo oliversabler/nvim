e0067a67a1c28b47e2b7119db50241635f078aac
oliversabler <oliver.sabler@gmail.com>
2025-04-06 21:13:28 +0200

    fix status column, abs rel sig

--- lua/chaebol/set.lua
+++ lua/chaebol/set.lua
@@ -1,8 +1,32 @@
 vim.opt.guicursor = ''
 
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.statuscolumn = '%l %=%r '
+vim.opt.statuscolumn = [[%!v:lua.StatusColumn()]]
+
+function StatusColumn()
+  local rel_num = vim.v.relnum
+  local abs_num = vim.v.lnum
+  local sign = vim.fn.sign_getplaced(vim.api.nvim_get_current_buf(), {
+    lnum = abs_num,
+    group = '*'
+  })[1].signs
+
+  local sign_col= " "
+  if sign and #sign > 0 then
+    local name = sign[1].name
+    if name:match("DapBreakpoint") then
+      sign_col = "●"
+    else
+      sign_col = " "
+    end
+  end
+
+  local rel_str = string.format('%3d', rel_num)
+  local abs_str = string.format('%3d', abs_num)
+
+  return string.format('%s%s %s ', abs_str, rel_str, sign_col)
+end
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -44,4 +68,4 @@ vim.opt.listchars:append {
     nbsp = space
 }
 
-vim.api.nvim_set_hl(0, "Whitespace", { fg = "#404040" })
+vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#404040' })
