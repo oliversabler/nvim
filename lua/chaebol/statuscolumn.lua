2c4a7734c61af4adf2f27014d474b75cede28a35
oliversabler <oliver.sabler@gmail.com>
2025-04-07 13:20:02 +0200

    refac statuscolumn

--- lua/chaebol/statuscolumn.lua
+++ lua/chaebol/statuscolumn.lua
@@ -1,24 +1,24 @@
 local statuscolumn = {}
 
-function statuscolumn.Set()
-    local rel_num = vim.v.relnum
-    local abs_num = vim.v.lnum
-    local bufnr = vim.api.nvim_get_current_buf()
+local function get_sign(abs_num, bufnr)
+    local dap_signs= {
+        DapBreakpoint = '●',
+        DapStopped = '▶',
+    }
 
-    local symbol = statuscolumn.set_diagnostic_symbol(abs_num, bufnr)
-    local signs = statuscolumn.get_signs(abs_num, bufnr)
+    local signs = vim.fn.sign_getplaced(bufnr, {
+        group = '*',
+        lnum = abs_num,
+    })[1].signs
 
-    if #signs > 0 then
-        symbol = statuscolumn.set_dap_symbol(symbol, signs)
+    for _, sign in ipairs(signs) do
+        for prefix, symbol in pairs(dap_signs) do
+            if sign.name:match(prefix) then
+                return symbol
+            end
+        end
     end
 
-    local rel_str = string.format('%3d', rel_num)
-    local abs_str = string.format('%3d', abs_num)
-
-    return string.format('%s%s %s', abs_str, rel_str, symbol)
-end
-
-function statuscolumn.set_diagnostic_symbol(abs_num, bufnr)
     local diags = vim.diagnostic.get(bufnr, { lnum = abs_num - 1 })
 
     if #diags > 0 then
@@ -27,38 +27,27 @@ function statuscolumn.set_diagnostic_symbol(abs_num, bufnr)
         end)
 
         local top_diag = diags[1]
-        local config = vim.diagnostic.config()
-        local signs = config.signs
-
-        if type(signs) == "table" and signs.text then
-            local icon = signs.text[top_diag.severity]
-            if icon then
-                return icon .. ' '
-            end
+        local icon = vim.diagnostic.config().signs.text[top_diag.severity]
+        if icon then
+            return icon .. ' '
         end
     end
 
-    return '  '
+    return ' '
 end
 
-function statuscolumn.get_signs(abs_num, bufnr)
-    return vim.fn.sign_getplaced(bufnr, {
-        group = '*',
-        lnum = abs_num,
-    })[1].signs
-end
+function statuscolumn.Set()
+    local rel_num = vim.v.relnum
+    local abs_num = vim.v.lnum
 
-function statuscolumn.set_dap_symbol(symbol, signs)
-    for _, sign in ipairs(signs) do
-        if sign.name:match("DapBreakpoint") then
-            symbol = '● '
-            break
-        elseif sign.name:match("DapStopped") then
-            symbol = '▶ '
-            break
-        end
-    end
-    return symbol
+    local bufnr = vim.api.nvim_get_current_buf()
+    local sign = get_sign(abs_num, bufnr)
+
+    local rel_str = string.format('%3d', rel_num)
+    local abs_str = string.format('%3d', abs_num)
+    local sign_str = string.format('%2s', sign)
+
+    return string.format('%s%s %s', abs_str, rel_str, sign_str)
 end
 
 return statuscolumn
