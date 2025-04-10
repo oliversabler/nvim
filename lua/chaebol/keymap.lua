fc01d1a7309c6d954c95e67fc44fe95e81d3b854
oliversabler <oliver.sabler@gmail.com>
2025-04-10 08:56:20 +0200

    fix copy clipboard keymaps

--- lua/chaebol/keymap.lua
+++ lua/chaebol/keymap.lua
@@ -24,5 +24,5 @@ vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
 
 -- Copy to clipboard
-vim.keymap.set("v", "<leader>y", "'+y")
-vim.keymap.set("n", "<leader>Y", "'+Y")
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
