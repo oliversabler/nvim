7d264d4888502c11fb54a50cf038fd21c7666b6d
oliversabler <oliver.sabler@gmail.com>
2025-04-23 18:41:10 +0200

    add lldb for c debugging

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -17,7 +17,40 @@ require "dapui".setup({
     }
 })
 
-local dap, dapui = require "dap", require "dapui"
+local dap = require "dap"
+
+-- LLDB
+dap.adapters.cppdbg = {
+    id = "cppdbg",
+    type = "executable",
+    command = vim.fn.expand("$HOME") .. "/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+    options = {
+        env = {
+            MIDebuggerPath = "/usr/bin/lldb"
+        }
+    }
+}
+
+dap.configurations.cpp = {
+    {
+        name = "Launch",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopAtEntry = true,
+        MIMode = "lldb",
+        MIDebuggerPath = '/usr/bin/lldb',
+    }
+}
+
+dap.configurations.c = dap.configurations.cpp
+
+
+-- UI ATTACH
+local dapui = require "dapui"
 dap.listeners.before.attach.dapui_config = function()
     dapui.open()
 end
@@ -31,7 +64,7 @@ dap.listeners.before.event_exited.dapui_config = function()
     dapui.close()
 end
 
--- TODO: Better keymaps
+-- KEYMAPS
 vim.keymap.set("n", "<F5>", function() require("dap").continue() end)
 vim.keymap.set("n", "<F10>", function() require("dap").step_over() end)
 vim.keymap.set("n", "<F11>", function() require("dap").step_into() end)
