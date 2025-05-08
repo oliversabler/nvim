7acaa42d958fcea041b1f631fd5c90ecea1fe6b5
oliversabler <oliver.sabler@gmail.com>
2025-05-08 09:07:23 +0200

    add terminate cmd for dap

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -80,28 +80,22 @@ dap.listeners.before.event_exited.dapui_config = function()
 end
 
 -- KEYMAPS
-vim.keymap.set("n", "<F5>", function() require("dap").continue() end)
-vim.keymap.set("n", "<F10>", function() require("dap").step_over() end)
-vim.keymap.set("n", "<F11>", function() require("dap").step_into() end)
-vim.keymap.set("n", "<F12>", function() require("dap").step_out() end)
+vim.keymap.set("n", "<F5>", function() dap.continue() end)
+vim.keymap.set("n", "<F10>", function() dap.step_over() end)
+vim.keymap.set("n", "<F11>", function() dap.step_into() end)
+vim.keymap.set("n", "<F12>", function() dap.step_out() end)
 
-vim.keymap.set("n", "<Leader>b", function() require("dap").toggle_breakpoint() end)
-vim.keymap.set("n", "<Leader>dr", function() require("dap").repl.open() end)
-
-vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
-    require("dap.ui.widgets").hover()
+vim.keymap.set("n", "<Leader>q", function()
+    dap.terminate()
+    dap.close()
+    dapui.close()
 end)
 
-vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
-    require("dap.ui.widgets").preview()
-end)
+vim.keymap.set("n", "<Leader>b", function() dap.toggle_breakpoint() end)
+vim.keymap.set("n", "<Leader>dr", function() dap.repl.open() end)
 
-vim.keymap.set("n", "<Leader>df", function()
-    local widgets = require("dap.ui.widgets")
-    widgets.centered_float(widgets.frames)
-end)
-
-vim.keymap.set("n", "<Leader>ds", function()
-    local widgets = require("dap.ui.widgets")
-    widgets.centered_float(widgets.scopes)
-end)
+local widgets = require "dap.ui.widgets"
+vim.keymap.set({ "n", "v" }, "<Leader>dh", function() widgets.hover() end)
+vim.keymap.set({ "n", "v" }, "<Leader>dp", function() widgets.preview() end)
+vim.keymap.set("n", "<Leader>df", function() widgets.centered_float(widgets.frames) end)
+vim.keymap.set("n", "<Leader>ds", function() widgets.centered_float(widgets.scopes) end)
